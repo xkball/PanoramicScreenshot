ae40744c1f1ad4dc71fd0c5b9b442d660db69708
xkball <45330674+xkball@users.noreply.github.com>
2026-02-12 02:45:04 +0800

    port to 1.20.1

--- src/main/java/com/xkball/panoramic_screenshot/PauseOverlay.java
+++ src/main/java/com/xkball/panoramic_screenshot/PauseOverlay.java
@@ -1,7 +1,7 @@
 package com.xkball.panoramic_screenshot;
 
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Overlay;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -10,7 +10,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class PauseOverlay extends Overlay {
     
     @Override
-    public void render(PoseStack poseStack, int i, int i1, float v) {
+    public void render(GuiGraphics guiGraphics, int i, int i1, float v) {
     
     }
 }
